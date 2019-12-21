b3f6d1b766f27eea169af33a4428d20355e2188a
wupeng <wupengpeng@zmeng123.com>
2019-12-21 10:53:01 +0800

    add hello

--- mv-service-hello/src/main/java/com/marvel/mvservicehello/MvServiceHelloApplication.java
+++ mv-service-hello/src/main/java/com/marvel/mvservicehello/MvServiceHelloApplication.java
@@ -2,9 +2,17 @@ package com.marvel.mvservicehello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class MvServiceHelloApplication {
+
+	@RequestMapping("/")
+	public String home() {
+		return "Hello world";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MvServiceHelloApplication.class, args);
