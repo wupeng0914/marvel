aa05e6e1a4289c1322ff14e8e2186ee37dc42d3f
wupeng <wupengpeng@zmeng123.com>
2019-12-27 14:09:14 +0800

    add hystrix dashboard for customer-feign

--- mv-customer-feign/src/main/java/com/marvel/mvcustomerfeign/MvCustomerFeignApplication.java
+++ mv-customer-feign/src/main/java/com/marvel/mvcustomerfeign/MvCustomerFeignApplication.java
@@ -2,10 +2,12 @@ package com.marvel.mvcustomerfeign;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients // 开启Feign 功能
+@EnableHystrixDashboard
 public class MvCustomerFeignApplication {
 
     public static void main(String[] args) {
