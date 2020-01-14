e6282f6ae61e5be233cece4866b2c61611b65bd3
wupeng <wupengpeng@zmeng123.com>
2020-01-14 14:11:58 +0800

    rocketmq demo

---
+++ mv-common/src/main/java/com/marvel/common/Demo.java
@@ -0,0 +1,9 @@
+package com.marvel.common;
+
+/**
+ * @Description
+ * @Author wupeng
+ * @Date 2020-01-02 11:34
+ **/
+public class Demo {
+}
---
+++ mv-zuul/src/main/java/com/marvel/mvzuul/filter/IFilter.java
@@ -0,0 +1,56 @@
+package com.marvel.mvzuul.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+/**
+ * @Description
+ * @Author wupeng
+ * @Date 2019-12-31 17:22
+ **/
+@Component
+public class IFilter extends ZuulFilter {
+
+    private static Logger log = LoggerFactory.getLogger(IFilter.class);
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext context = RequestContext.getCurrentContext();
+        HttpServletRequest request = context.getRequest();
+        log.info(String.format("%s >>> %s", request.getMethod(), request.getRequestURL().toString()));
+        String token = request.getParameter("token");
+        if (token == null){
+            log.warn("token is empty");
+            context.setSendZuulResponse(false);
+            context.setResponseStatusCode(401);
+            try {
+                context.getResponse().getWriter().write("token is empty");
+            } catch (IOException e) {}
+            return null;
+        }
+        log.info("ok");
+        return null;
+    }
+}
